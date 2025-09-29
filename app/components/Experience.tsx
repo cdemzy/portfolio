8a67f0a19c90f1ba2668c8ecd952e89e32900733
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-28 22:31:31 -0300

    feat: add Experience component to showcase user experience details

---
+++ app/components/Experience.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Experience() {
+  return (
+    <section className="index-section" id="experience">
+      <h2>Experience</h2>
+      <ul className="text-secondary">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </section>
+  )
+}
