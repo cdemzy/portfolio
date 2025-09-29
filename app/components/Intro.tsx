55fd10579c48e7f37d2fc8c029219731aa9925bc
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-28 22:31:16 -0300

    feat: add Intro component with introductory text and styling

---
+++ app/components/Intro.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Intro() {
+  return (
+    <section className="index-section" id="Intro">
+      <h1>Charles Dela Cruz</h1>
+      <article className="text-secondary">
+        Hello World!
+      </article>
+    </section>
+  )
+};
