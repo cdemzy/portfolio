b3f6617ee2d1d1c5fca2f1a3ee68e0a4b1ff1a00
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-29 23:30:37 -0300

    feat: add Education component to showcase academic background

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import Intro from "./components/Intro"
 import Experience from "./components/Experience";
 import Connect from "./components/Connect";
+import Education from "./components/Education";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-12">
       <Intro/>
       <Experience/>
+      <Education/>
       <Connect/>
     </main>
   );
