e98d49f5beba4cd8bf7facc30ab2489d9e2e8881
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-29 22:19:49 -0300

    style: update Connect component styles for social links with improved hover effects and layout

--- app/components/Connect.tsx
+++ app/components/Connect.tsx
@@ -19,11 +19,11 @@ export default function Connect() {
 	return (
 		<section className="index-section" id="connect">
 			<h2>Connect</h2>
-			<ul className="flex flex-col gap-6">
+			<ul className="w-min flex flex-col gap-6 group">
 				{socialLinks.map((link) => {
 					const IconComponent = link.icon;
 					return (
-						<li className="underline underline-offset-4" key={link.name}>
+						<li className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-400 cursor-pointer" key={link.name}>
 							<a 
 								href={link.url} 
 								target="_blank" 
@@ -31,7 +31,7 @@ export default function Connect() {
 								className="social-link flex gap-1"
 							>
 								<IconComponent size={20} />
-								{link.name}
+								<span className="underline underline-offset-4 dark:decoration-gray-700 hover:decoration-foreground duration-400">{link.name}</span>
 							</a>
 						</li>
 					);
