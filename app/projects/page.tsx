76cb95844e783c45b5d5c89ff188230c81137c34
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-30 20:21:46 -0300

    feat: create Projects page with placeholder content for future updates

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function page() {
+    return (
+        <main className="flex flex-col gap-12">
+            <h1>Projects</h1>
+            <p className="text-2xl text-center font-bold">Coming Soon...</p>
+        </main>
+    )
+}
