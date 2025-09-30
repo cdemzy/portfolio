fed109cb74d39f10e8187e49c994e7667deb7862
Charles <119467519+cdemzy@users.noreply.github.com>
2025-09-29 23:11:03 -0300

    style: refine global CSS variables and update Intro component for consistent secondary color usage

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="index-section" id="Intro">
       <h1>Charles Dela Cruz</h1>
-      <article className="text-secondary">
+      <article className="text-secondary-foreground dark:text-foreground-dark">
         <p className="textbase">
           I'm a third-year Computer Science student at Dalhousie University.
           As a software developer, I focus on building elevated user experiences by creating applications and projects that bring ideas to life.
