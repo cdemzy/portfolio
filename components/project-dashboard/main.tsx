12ecc33b4ec2f75898cb62b994df272f32fdbe01
Charles <119467519+cdemzy@users.noreply.github.com>
2025-10-01 00:46:24 -0300

    chore: remove all 1.1

--- components/project-dashboard/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MainProjectDashboard() {
-  return (
-    <main className='h-full w-full'><h1 className='text-lg'>Coming Soon</h1></main>
-  )
-}
